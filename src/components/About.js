80c187124fc9f68bf76dd76dc264185ef30441dc
Michael Prieto <prieto4e@gmail.com>
2021-07-28 14:20:59 -0500

    updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,11 @@ const About = () => {
         <h1>About</h1>
         <p>
           Observe and learn about the planets of the Solar System. Take the quiz
-          and see how you stack up against others and your past high scores!
+          and see how you stack up against others and your past high scores. If
+          "Day Count" is enabled, each planet has a green number next to it
+          representing the relative day lengths of each planet. One second is
+          equal to 10 Earth days in the animation. Created with love by Michael
+          Prieto.
         </p>
       </div>
     </a>
