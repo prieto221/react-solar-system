6d7cd072a297fc0c0a604bf0295a77fe253884a8
Michael Prieto <prieto4e@gmail.com>
2021-08-08 20:38:10 -0500

    css

--- src/components/About.js
+++ src/components/About.js
@@ -13,11 +13,10 @@ const About = () => {
         <div className='about-container'>
           <h1>About</h1>
           <p className='about-text'>
-            Observe and learn about the planets of the Solar System. Take the
-            quiz and see how you stack up against others and your past high
-            scores. If "Day Count" is enabled, each planet has a green number
-            next to it representing the relative day lengths of each planet. One
-            second is equal to 10 Earth days in the animation.
+            Observe and learn about the planets of the Solar System. If "Day
+            Count" is enabled, each planet has a green number next to it
+            representing the relative day lengths of each planet. One second is
+            equal to 10 Earth days in the animation.
           </p>
         </div>
       </div>
