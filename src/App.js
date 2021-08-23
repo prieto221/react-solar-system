e9f3c051a504ad93314718d0c0350ff9aaecc0e5
Michael Prieto <prieto4e@gmail.com>
2021-08-23 12:52:38 -0500

    changed planet starting locations

--- src/App.js
+++ src/App.js
@@ -27,6 +27,13 @@ import MobileNavLinks from './components/MobileNavLinks';
 import HamburgerMenu from './components/HamburgerMenu';
 import RightSide from './components/RightSide';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
 function App() {
   const [selected, setSelected] = useState(marsData);
   const [showSettings, setShowSettings] = useState(false);
